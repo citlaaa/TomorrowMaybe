a0c2423d3c2af3ae60fdb9caa236314092815b72
Citlally Suarez <citlally.suarez@comunidad.unam.mx>
2025-06-11 11:16:58 -0600

    Se corrigio la navegacion y problemas en AndroidManifest.

--- app/src/main/java/com/example/tomorrowmaybe/ListActivity.kt
+++ app/src/main/java/com/example/tomorrowmaybe/ListActivity.kt
@@ -1,15 +1,20 @@
 package com.example.tomorrowmaybe
 
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.tomorrowmaybe.R
 import com.example.tomorrowmaybe.databinding.ActivityListBinding
+import com.example.tomorrowmaybe.databinding.ActivityOnboardingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ListActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +23,5 @@ class ListActivity : AppCompatActivity() {
         binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_list)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
